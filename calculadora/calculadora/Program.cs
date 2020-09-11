2f7f0a6b5f265bd4765c8d676862c98e161b033c
jaredtovar2390 <70724012+jaredtovar2390@users.noreply.github.com>
2020-09-11 00:29:25 -0500

    Add files via upload

---
+++ calculadora/calculadora/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            //preguntar el valor del primer intervalo
+            Console.Write("a: ");
+            try
+            {
+                a = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("dato invalido" + x);
+                Console.ReadKey();
+                return;
+                throw;
+            }
+           
+            // preguntar el valor del segundo intervalo
+            Console.Write("b: ");
+            b = Int32.Parse(Console.ReadLine());
+            //sumar intervalo
+            c = a + b;
+            Console.Write("la suma es" + c);
+            Console.ReadKey();
+
+        }
+    }
+}
